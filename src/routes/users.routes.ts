b272454c5eac4d4a4b96bc39ba5c16574d7a26c3
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2023-03-15 23:22:27 +0000

    req 7

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 
 import * as userController from '../controllers/user.controller';
+import * as validation from '../middlewares/validationUser';
 
 const router = Router();
 
-router.post('/', userController.create);
+router.post(
+  '/', 
+  validation.validateUserName,
+  validation.validateVocation, 
+  validation.validateLevel, 
+  validation.validatePassword, 
+  userController.create,
+);
 // router.get('/', userController.getAll);
 
 export default router;
